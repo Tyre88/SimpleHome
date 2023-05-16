962ead83e5ab387211dc8d041ae0552bf36afeee
Victor Öhrström <victor@phoenixsolutions.se>
2023-05-16 19:03:24 +0200

    fix: fixing slider

--- app/components/DialogComponents/light-dialog-component.tsx
+++ app/components/DialogComponents/light-dialog-component.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { FC, useEffect, useState } from "react";
-import { Dimensions, Slider, StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import Slider from '@react-native-community/slider';
 import { DialogComponentProps } from "./dialog-component-props";
 import ColorPicker, { Panel3, Swatches, Preview, colorKit } from 'reanimated-color-picker';
 import { hassEntityService } from "../../services/hass/hass-entity-service";
@@ -8,6 +9,7 @@ import { hassEntityService } from "../../services/hass/hass-entity-service";
 export const LightDialogComponent: FC<DialogComponentProps> = observer(function LightDialogComponent(props: DialogComponentProps) {    
     const [color, setColor] = useState<string>("#FFFFFF");
     const [brightness, setBrightness] = useState<number>(0);
+    const [isColorModeSupported, setIsColorModeSupported] = useState<boolean>(false);
 
     console.log(props.hassEntity)
 
@@ -31,11 +33,6 @@ export const LightDialogComponent: FC<DialogComponentProps> = observer(function 
             console.log('CHANGED COLOR');
             console.log(d);
         });
-
-        // hassEntityService.setState(state).then((d) => {
-        //     console.log('CHANGED COLOR');
-        //     console.log(d);
-        // });
     }
 
     useEffect(() => {
@@ -46,21 +43,45 @@ export const LightDialogComponent: FC<DialogComponentProps> = observer(function 
         if(props.hassEntity.attributes.brightness) {
             setBrightness(props.hassEntity.attributes.brightness);
         }
+
+        setColorModeSupported();
     }, []);
+
+    const setColorModeSupported = () => {
+        if(props.hassEntity.attributes.supported_color_modes) {
+            const isColorSupported = props.hassEntity.attributes.supported_color_modes.includes("hs");
+            setIsColorModeSupported(isColorSupported);
+        }
+    }
 
     const updateBrightness = (value: number) => {
         console.log('UPDATE BRIGHTNESS', value);
         setBrightness(value);
+
+        const state = {
+            entity_id: props.hassEntity.entity_id,
+            brightness: value
+        };
+
+        const action = value > 0 ? "turn_on" : "turn_off";
+
+        console.log(state);
+
+        hassEntityService.callService("light", action, state).then((d) => {
+            console.log('CHANGED BRIGHTNESS');
+            console.log(d);
+        });
     }
 
     return (
         <View>
-            <ColorPicker style={{ width: 300 }} value={color} onChange={onColorChange} onComplete={onColorChangeComplete}>
+            { isColorModeSupported ? <ColorPicker style={{ width: 300 }} value={color} onChange={onColorChange} onComplete={onColorChangeComplete}>
                 <Preview hideInitialColor={true} />
                 <Panel3 style={{marginTop: 15}} />
                 <Swatches style={{marginTop: 15}} />
-            </ColorPicker>
-            <Slider minimumValue={0} maximumValue={255} step={1} value={brightness} onValueChange={updateBrightness}></Slider>
+            </ColorPicker> : null}
+            <Slider style={{ width: 300 }} minimumValue={0} maximumValue={255} step={1} value={brightness} 
+                onSlidingComplete={updateBrightness}></Slider>
         </View>
     );
 });
